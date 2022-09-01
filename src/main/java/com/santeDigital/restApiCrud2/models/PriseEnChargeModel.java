d9558aed0790e6a0c50f62a5e1242d75c369f075
brahim <brahimidmansour015@gmail.com>
2022-09-01 23:45:22 +0100

    prise en charge  Entity

--- src/main/java/com/santeDigital/restApiCrud2/models/PriseEnChargeModel.java
+++ src/main/java/com/santeDigital/restApiCrud2/models/PriseEnChargeModel.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "priseEnCharge")
+@Document(collection = "prise_en_charge")
 public class PriseEnChargeModel {
     @Id
     private String id;
