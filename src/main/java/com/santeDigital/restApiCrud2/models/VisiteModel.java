8c3a5176bd8c6f5b40965463da65a8b30dc69926
brahim <brahimidmansour015@gmail.com>
2022-09-06 12:27:50 +0100

    visite model adding date createdAt

--- src/main/java/com/santeDigital/restApiCrud2/models/VisiteModel.java
+++ src/main/java/com/santeDigital/restApiCrud2/models/VisiteModel.java
@@ -1,17 +1,20 @@
 package com.santeDigital.restApiCrud2.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
 import java.util.Set;
 
 @Data
 @Document(collection = "visite")
+@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)
 public class VisiteModel {
     @Id
     private String id;
-    private String date;
+    private Date createdAt = new Date();
     private String service;
     private String etat;
     private Set<VisiteMaladies> maladies;
