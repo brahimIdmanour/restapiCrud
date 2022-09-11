312d7bd3d1753feb47fe797d1fb3baf90b9ebb71
brahim <brahimidmansour015@gmail.com>
2022-09-11 23:33:56 +0100

    conclusion clinique (array List)

--- src/main/java/com/santeDigital/restApiCrud2/models/ConclusionCliniqueModel.java
+++ src/main/java/com/santeDigital/restApiCrud2/models/ConclusionCliniqueModel.java
@@ -5,13 +5,15 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "conclusion_clinique")
 public class ConclusionCliniqueModel {
     @Id
     private String id;
-    private String syndromes;
-    private String symptomes;
+    private List<String> syndromes;
+    private List<String>  symptomes;
     @DBRef
     private VisiteModel visiteRefId;
 }
