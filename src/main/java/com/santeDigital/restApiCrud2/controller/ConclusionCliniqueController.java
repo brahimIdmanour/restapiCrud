bb8b4e9ae243bce2312b37d173156629eba1e710
ABDELBAKI-Mohamed <abdelbaki.mo6@gmail.com>
2022-09-20 19:13:27 +0100

    Update ConclusionCliniqueController.java

--- src/main/java/com/santeDigital/restApiCrud2/controller/ConclusionCliniqueController.java
+++ src/main/java/com/santeDigital/restApiCrud2/controller/ConclusionCliniqueController.java
@@ -16,30 +16,36 @@ import java.util.Optional;
 public class ConclusionCliniqueController {
     @Autowired
     private ConclusionCliniqueService conclusionCliniqueService;
+
     @GetMapping("/findAll")
-    public ResponseEntity<List<ConclusionCliniqueModel>> findAll(){
+    public ResponseEntity<List<ConclusionCliniqueModel>> findAll() {
         return ResponseEntity.ok(conclusionCliniqueService.findAll());
     }
+
     @GetMapping("findById/{id}")
     public ResponseEntity<ConclusionCliniqueModel> findById(@PathVariable("id") String id) {
         Optional<ConclusionCliniqueModel> conclusionById = conclusionCliniqueService.findById(id);
-        return new  ResponseEntity<>(conclusionById.get(), HttpStatus.OK);
+        return new ResponseEntity<>(conclusionById.get(), HttpStatus.OK);
     }
-   @GetMapping("/findByvisiteId/{id}")
-   public  ResponseEntity<ConclusionCliniqueModel> findByVisiteId(@PathVariable("id") String id){
+
+    @GetMapping("/findByvisiteId/{id}")
+    public ResponseEntity<ConclusionCliniqueModel> findByVisiteId(@PathVariable("id") String id) {
         Optional<ConclusionCliniqueModel> conclusionByVisiteId = conclusionCliniqueService.findByVisiteId(id);
         return new ResponseEntity<>(conclusionByVisiteId.get(), HttpStatus.OK);
-   }
+    }
+
     @PostMapping("/save")
-    public ResponseEntity<ConclusionCliniqueModel> save(@RequestBody ConclusionCliniqueModel request){
+    public ResponseEntity<ConclusionCliniqueModel> save(@RequestBody ConclusionCliniqueModel request) {
         return ResponseEntity.ok(conclusionCliniqueService.save(request));
     }
-    @PutMapping("/update/{id}")
-    public ResponseEntity<ConclusionCliniqueModel> update(@PathVariable("id") String id, @RequestBody ConclusionCliniqueModel request){
+
+    @PutMapping("/update")
+    public ResponseEntity<ConclusionCliniqueModel> update(@RequestBody ConclusionCliniqueModel request) {
         return ResponseEntity.ok(conclusionCliniqueService.update(request));
     }
+
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ConclusionCliniqueModel> delete(@PathVariable("id") String id){
+    public ResponseEntity<ConclusionCliniqueModel> delete(@PathVariable("id") String id) {
         conclusionCliniqueService.delete(id);
         return ResponseEntity.ok().build();
     }
